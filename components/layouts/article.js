ab9574bb6c2bc9c01026d15607f3057e5afcf37f
yingyangcode <marcel.anis.21@gmail.com>
2021-10-17 17:58:13 -0400

    remove unused imports

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import Head from 'next/head'
-import { GridItem, GridItemStyle } from '../grid-item'
+import { GridItemStyle } from '../grid-item'
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
